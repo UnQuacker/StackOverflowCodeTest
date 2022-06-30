c2d370c6612e2ce55c33e7af63c2b8c00b8af3f4
Talgat Sovetbekov <sarkytbayevquacker@gmail.com>
2022-06-30 15:26:36 +0600

    FINALLY

--- StackOverFlowCodeTestProject/StackOverFlowCodeTestProject.cpp
+++ StackOverFlowCodeTestProject/StackOverFlowCodeTestProject.cpp
@@ -11,10 +11,6 @@ int win_error()
 }
 
 int main()
-// что по задумке должен программа должны была делать:
-// открыть внешний носитель -> создать временный файл -> заполнить данными -> считать его байты -> удалить временный файл -> переместить указатель записи внешнего носителя (hDisk) -> записать туда сохранённый байты
-
-// но программа выдаёт ошибку "Unhandled exception at 0x75C4C3A2 in StackOverFlowCodeTestProject.exe: Microsoft C++ exception: int at memory location 0x009CF2F4." на 47 строке
 {
     BOOL fSuccess = false;
 
@@ -23,44 +19,24 @@ int main()
     if (hDisk == INVALID_HANDLE_VALUE) { throw win_error(); };
 
     BYTE buf[512];
-    BYTE buffer[512];
+    //BYTE buffer[512];
     DWORD dwRead;
-    DWORD dwRead1;
-    char chBuffer[16]="abcdefghijklmno";
-    long SectorActual = 70;  // номер сектора
+    //DWORD dwRead1;
+    char chBuffer[512]="ABCDEFGHIJKlMNO"; 
+    char chBuffer1[512] = "abcdefghijklmno";
+    char chBuffer2[16] = "abcdefghijklmno";
+    long SectorActual = 125;  // номер сектора
     long PosicionInicio = SectorActual * 512;
     long SectorMove = SectorActual * 512;
+    BOOL size = true;
 
     long numberOfBytes = sizeof(chBuffer);
-
-    //создание временного файла
-    /*HANDLE hFile = ::CreateFile(R"(\\.\D:\\WriteHere.txt)", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hFile == INVALID_HANDLE_VALUE) { throw win_error(); return 0; };*/
+    int numberOfTries = 0;
 
     DWORD dwBytesWritten = 0;
     DWORD dwBytesWritten1 = 0;
-
-    //запись данных в временный файл
-    //fSuccess = WriteFile(hFile, chBuffer, numberOfBytes, &dwBytesWritten, NULL);
-
-    //попытка прочитать байты файла, тут и выходит ошибка
-    /*try {
-        if (!::ReadFile(hFile, buffer, sizeof(buffer), &dwRead1, NULL)) { cout << GetLastError()<<endl; return 0; };
-    }*/
-
-    /*catch (exception e) {
-        cout << GetLastError();
-    }*/
-
-    //::CloseHandle(hFile);
-
-    //удаление временного файла
-    //fSuccess = DeleteFile("D:\\WriteHere.txt"); 
-    /*if (!fSuccess)
-    {
-        printf("DeleteFile failed (%d)\n", GetLastError());
-        return (6);
-    }*/
+    DWORD dwBytesWritten2 = 0;
+    DWORD dwBytesWritten3 = 0;
 
     //двигаем указатель диска
     DWORD dwPtr = SetFilePointer(hDisk,
@@ -68,17 +44,15 @@ int main()
         NULL,
         FILE_BEGIN);
 
-    //запись сохранённых данных на диск
+    //запись данных на диск
     fSuccess = ::WriteFile(hDisk, chBuffer, sizeof(chBuffer), &dwBytesWritten1, NULL);
-    if (!fSuccess) {
-        cout << "Fail" << endl;;
-        cout << "Last error code: " << GetLastError() << endl;
-    }
-
-   
+    DWORD dwPtr2 = SetFilePointer(hDisk,
+        SectorMove,
+        NULL,
+        FILE_BEGIN);
 
     //чтение байтов диска
-    if (!::ReadFile(hDisk, buf, sizeof(buf), &dwRead, NULL) || dwRead != sizeof(buf)) { throw win_error(); return 0; };
+    if (!::ReadFile(hDisk, buf, sizeof(buf), &dwRead, NULL) || dwRead != sizeof(buf)) { cout << "Error"; };
     ::CloseHandle(hDisk);
 
     ofstream out(R"(\\.\C:\\Users\\Alser\\Desktop\\fileoutput.txt)");
@@ -87,7 +61,7 @@ int main()
     {
         char f[3];  _itoa(buf[i], f, 16);
         out << i << "\t" << f << "\t" << buf[i] << '\n';
-        cout << buf[i];
+        cout <<buf[i];
     }
 
     out.close();
